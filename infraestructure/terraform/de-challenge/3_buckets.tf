083fffc37f206ceeda2b30b98cc32afda824893c
zahid <zahid.galea@wom.cl>
2021-12-28 15:36:28 -0300

    Adds infraestructure into prod

--- infraestructure/terraform/de-challenge/3_buckets.tf
+++ infraestructure/terraform/de-challenge/3_buckets.tf
@@ -1,3 +1,7 @@
+################
+# Buckets
+################
+
 module "landing-bucket-metascore" {
   source      = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version     = "~> 1.3"
@@ -8,22 +12,90 @@ module "landing-bucket-metascore" {
   iam_members = concat(local.metascore-owners-access, local.metascore-editor-access, local.metascore-viewer-access)
 }
 
+
+module "raw-bucket-metascore" {
+  source      = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
+  version     = "~> 1.3"
+  name        = "${local.project_id}-${local.metascore-raw}"
+  project_id  = local.project_id
+  location    = local.default_region
+  versioning  = true
+  iam_members = concat(local.metascore-owners-access, local.metascore-editor-access, local.metascore-viewer-access)
+}
+
+module "analytics-bucket-metascore" {
+  source      = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
+  version     = "~> 1.3"
+  name        = "${local.project_id}-${local.metascore-analytics}"
+  project_id  = local.project_id
+  location    = local.default_region
+  versioning  = true
+  iam_members = concat(local.metascore-owners-access, local.metascore-editor-access, local.metascore-viewer-access)
+}
+
+################
+# Landing Notification
+################
+
+# GCS Landing notification to pubsub
+resource "google_storage_notification" "object_landing_notification" {
+  provider       = google
+  bucket         = "${local.project_id}-${local.metascore-ldn}"
+  payload_format = "JSON_API_V1"
+  topic          = google_pubsub_topic.ldn_arrive_topic.name
+  event_types    = [
+    "OBJECT_FINALIZE"
+  ]
+  depends_on     = [
+    module.landing-bucket-metascore,
+    google_pubsub_topic.ldn_arrive_topic,
+    google_pubsub_topic_iam_binding.ldn_arrive_binding
+  ]
+}
+
+resource "google_pubsub_topic" "ldn_arrive_topic" {
+  provider = google
+  name     = local.metascore-ldn-pubsub-topic
+  project  = local.project_id
+}
+
+data "google_storage_project_service_account" "ldn_gcs_account" {}
+
+# Pubsub iam binding
+resource "google_pubsub_topic_iam_binding" "ldn_arrive_binding" {
+  topic      = google_pubsub_topic.ldn_arrive_topic.name
+  role       = "roles/pubsub.publisher"
+  members    = [
+    "serviceAccount:${data.google_storage_project_service_account.ldn_gcs_account.email_address}"
+  ]
+  depends_on = [
+    google_pubsub_topic.ldn_arrive_topic
+  ]
+}
+
+
+################
+# Variables
+################
+
 locals {
-  metascore-ldn = "metascore-lnd"
-  metascore-raw = "metascore-raw"
-  metascore-owners-access = [
+  metascore-ldn              = "metascore-lnd"
+  metascore-ldn-pubsub-topic = "metascore-lnd-notification"
+  metascore-raw              = "metascore-raw"
+  metascore-analytics        = "metascore-analytics"
+  metascore-owners-access    = [
   for owner in local.owner_users : {
     role   = "roles/storage.objectAdmin",
     member = "user:${owner}"
   }
   ]
-  metascore-editor-access = [
+  metascore-editor-access    = [
   for editor in local.editor_users : {
     role   = "roles/storage.objectAdmin",
     member = "user:${editor}"
   }
   ]
-  metascore-viewer-access = [
+  metascore-viewer-access    = [
   for viewer in local.viewer_users : {
     role   = "roles/storage.objectViewer",
     member = "user:${viewer}"
