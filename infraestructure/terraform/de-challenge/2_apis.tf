a706a488c247256fad41f849ae8f7990f4bdfcc9
zahid <zahid.galea@wom.cl>
2021-12-28 02:20:42 -0300

    It adds api enablement on each project

---
+++ infraestructure/terraform/de-challenge/2_apis.tf
@@ -0,0 +1,20 @@
+resource "google_project_service" "project" {
+  for_each = local.apis
+  project  = local.project_id
+  service  = each.value
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
+
+locals {
+  apis = [
+    "iam.googleapis.com", "iamcredentials.googleapis.com", "cloudbuild.googleapis.com", "appengine.googleapis.com",
+    "cloudfunctions.googleapis.com", "compute.googleapis.com", "container.googleapis.com", "datastudio.googleapis.com",
+    "storage-component.googleapis.com", "bigquery.googleapis.com", "dataflow.googleapis.com"
+  ]
+}
