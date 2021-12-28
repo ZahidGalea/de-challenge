5dda7c04cb108000d09ddaae88c45e4cca777493
zahid <zahid.galea@wom.cl>
2021-12-28 15:43:09 -0300

    Adds a new dataset and fixes wrong name

--- infraestructure/terraform/de-challenge-test/4_datasets.tf
+++ infraestructure/terraform/de-challenge-test/4_datasets.tf
@@ -1,6 +1,7 @@
 module "analytics_dataset" {
+  for_each   = local.analytics-datasets
   source     = "terraform-google-modules/bigquery/google"
-  dataset_id = local.analytics-dataset
+  dataset_id = each.value
   project_id = local.project_id
   location   = "US"
   access     = []
@@ -9,8 +10,9 @@ module "analytics_dataset" {
 
 
 module "analytics_dataset_data_editor" {
+  for_each   = local.analytics-datasets
   source     = "../custom_modules/bigquery_iam"
-  dataset_id = local.analytics-dataset
+  dataset_id = each.value
   role       = "roles/bigquery.dataEditor"
   project_id = local.project_id
   members    = local.metascore-bq-editor-access
@@ -20,8 +22,9 @@ module "analytics_dataset_data_editor" {
 }
 
 module "analytics_dataset_data_owner" {
+  for_each   = local.analytics-datasets
   source     = "../custom_modules/bigquery_iam"
-  dataset_id = local.analytics-dataset
+  dataset_id = each.value
   role       = "roles/bigquery.dataOwner"
   project_id = local.project_id
   members    = local.metascore-bq-owners-access
@@ -31,8 +34,9 @@ module "analytics_dataset_data_owner" {
 }
 
 module "analytics_dataset_data_viewer" {
+  for_each   = local.analytics-datasets
   source     = "../custom_modules/bigquery_iam"
-  dataset_id = local.analytics-dataset
+  dataset_id = each.value
   role       = "roles/bigquery.dataViewer"
   project_id = local.project_id
   members    = local.metascore-bq-viewers-access
@@ -43,7 +47,7 @@ module "analytics_dataset_data_viewer" {
 
 
 locals {
-  analytics-dataset           = "analytics-dataset"
+  analytics-datasets          = ["staging", "consumption"]
   metascore-bq-owners-access  = [for owner in local.owner_users : "user:${owner}"]
   metascore-bq-viewers-access = [for viewer in local.viewer_users : "user:${viewer}"]
   metascore-bq-editor-access  = [for editor in local.editor_users : "user:${editor}"]
